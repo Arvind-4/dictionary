49f1e7033741d7ea2ad2d518a020b718e4bba46c
Arvind-4 <lordarvindthegamer@gmail.com>
2023-11-10 19:52:29 +0530

    Updated: config files for the project

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 80,
+  useTabs: false,
+  endOfLine: "auto",
+};
+export default config;
